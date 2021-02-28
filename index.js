2ff93f776673e5e7eee61a0f875c0027479f4897
lukascobit <77848587+lukascobit@users.noreply.github.com>
2021-02-28 13:56:11 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,50 @@
 
 function Translate(){
     
-    let phrase =("what")
+    
     let output = document.getElementById("output").value = document.getElementById("input").value; 
     let newstring = output.split(" ")
     
 
     console.log(newstring)
 
-
+    //newstring = document.getElementById("output").value = newstring.toString().replace(',',' ');
     
 
     
-    var arrayLength = newstring.length;
-    for (var i = 0; i < arrayLength; i++) {
+    var arrayLength = 50
+
+    for (let i = 0; i < arrayLength;i++) {
         
         if(newstring[i] == "Im"){
             newstring[i] = "I am"
         }
+        
+        else if (newstring[i] == "Thats"){
+            newstring[i] = "That is"
+
+        }
+        else if (newstring[i] == "Theres"){
+            newstring[i] = "There is"
+
+        }
+        else if (newstring[i] == "Whos"){
+            newstring[i] = "Who is"
+
+        }
+        else{
+            newstring[i]
+        
+        }
+        newstring = document.getElementById("output").value = newstring.toString().replace(',',' ');
+
+        
+        console.log(i)
+
         console.log(newstring[i]);
     }
     
-    //const replaced = newstring.replace(/,/g,"hi");
     
-    output = document.getElementById("output").value = newstring;
-
     
 
 
