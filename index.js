7125417b15270afb40fa5f6df631c1f3ade5c3ff
Lukas Odehnal <lukyn.odehnal333@gmail.com>
2021-02-28 10:45:36 +0100

    Im change

--- index.js
+++ index.js
@@ -1,6 +1,35 @@
 
 function Translate(){
-    document.getElementById("output").value = document.getElementById("input").value; 
+    
+    let phrase =("what")
+    let output = document.getElementById("output").value = document.getElementById("input").value; 
+    let newstring = output.split(" ")
+    
+
+    console.log(newstring)
 
 
+    
+
+    
+    var arrayLength = newstring.length;
+    for (var i = 0; i < arrayLength; i++) {
+        
+        if(newstring[i] == "Im"){
+            newstring[i] = "I am"
+        }
+        console.log(newstring[i]);
+    }
+    
+    //const replaced = newstring.replace(/,/g,"hi");
+    
+    output = document.getElementById("output").value = newstring;
+
+    
+
+
+   if (output == ""){
+    alert("Please enter a valid text")
+   }
 }
+
